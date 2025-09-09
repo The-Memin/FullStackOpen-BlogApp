7eed3ee4fcaae19d67d3d77c389d852f9626c478
The-Memin <guillejuma@gmail.com>
2025-09-09 14:28:59 -0600

    Part 5 - Exercise 5.28: Blog List End To End Testing, step 2 - Cypress

--- cypress-e2e-tests/cypress/e2e/blog_app.cy.js
+++ cypress-e2e-tests/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,19 @@
+const TEST_USER = {
+  name: 'Test User',
+  username: 'test-user',
+  password: 'password123**'
+}
 
 describe('Blog app', function(){
   beforeEach(function() {
+    cy.request('POST', '/api/testing/reset')
+    const user = {
+      name: TEST_USER.name,
+      username: TEST_USER.username,
+      password: TEST_USER.password
+    }
+    cy.request('POST', '/api/users/', user)
+
     cy.visit('/')
   })  
   it('Login form is shown', function() {
@@ -8,5 +21,30 @@ describe('Blog app', function(){
     cy.contains('login').click()
     cy.get('#username').should('be.visible')
     cy.get('#password').should('be.visible')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type(TEST_USER.username)
+      cy.get('#password').type(TEST_USER.password)
+      cy.get('button[type="submit"]').click()
+
+      cy.contains(`${TEST_USER.name} logged in`)
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type(TEST_USER.username)
+      cy.get('#password').type('wrong')
+      cy.get('button[type="submit"]').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', `${TEST_USER.name} logged in`)
+    })
   })
 })
